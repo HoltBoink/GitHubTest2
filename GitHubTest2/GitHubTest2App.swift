d4b03fffd65772b883ba5a1b202af4d2d025557b
HoltBoink <boinkh@bc.edu>
2023-01-23 14:29:22 -0500

    Initial Commit

---
+++ GitHubTest2/GitHubTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTest2App.swift
+//  GitHubTest2
+//
+//  Created by Holt Boink on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
